4932bc8c2ee1397e52dfe3a09b57c8c2b6ed2547
Italo Vizaco <italovizaco@gmail.com>
2023-01-17 11:21:13 -0300

    Phrases creator exercise

--- Arrays/js/script.js
+++ Arrays/js/script.js
@@ -1,2 +1,19 @@
-// array exercises 01
+// array exercises 01 creating an array
+
+let flavors = ["vanilla", "butterscotch", "lavander", "chocolate", "cookie dough"]
+
+// accessing a item inside an array
+
+let flavorOfTheDay = flavors[2]
+console.log(flavorOfTheDay);
+
+//updating an array value
+
+flavors[3] = "vanilla chocolate chip"
+
+//discovering a size of an array
+
+let numFlavors = flavors.length
+console.log(flavors.length);
+console.log(numFlavors);
 
